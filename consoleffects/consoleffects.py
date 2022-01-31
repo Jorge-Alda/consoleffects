5a6dcc57f0f2806305d6cbbcbe16bdd206c47fac
Jorge Alda Gallo <jalda@unizar.es>
2022-01-31 17:42:49 +0100

    Documentation

--- consoleffects/consoleffects.py
+++ consoleffects/consoleffects.py
@@ -1,3 +1,7 @@
+'''
+Code to decorate an string and show flashy effects on terminals.
+'''
+
 opening = '\x1b['
 closing = '\x1b[0m'
 
@@ -10,6 +14,22 @@ def decorate(source: str, *,
              blink: bool = False,
              strike: bool = False,
              duline: bool = False) -> str:
+    """Decorates a string using ANSI Escape sequences
+
+    Args:
+        source (str): Original string
+        bold (bool, optional): Bold text. Defaults to False.
+        dim (bool, optional): Dim text. Defaults to False.
+        it (bool, optional): Italics text. Defaults to False.
+        uline (bool, optional): Underlined text. Defaults to False.
+        blink (bool, optional): Blinking text. Defaults to False.
+        strike (bool, optional): Strike-through text. Defaults to False.
+        duline (bool, optional): Double-underlined text. Defaults to False.
+
+    Returns:
+        str: Decorated string
+    """
+
     args = []
     if bold:
         args.append('1')
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. consoleffects documentation master file, created by
+   sphinx-quickstart on Mon Jan 31 16:34:44 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to consoleffects's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   consoleffects
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
